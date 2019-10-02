4d137d2bd90a75a6aa3d7863a170e0aa71b19d87
Carpano Romuald <doc.roms@gmail.com>
2019-10-02 23:11:38 +0200

    feat(css): try to find a correct style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,6 +13,18 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/{locale}", name="home_international")
+     */
+    public function homeInternational(Request $request, $locale)
+    {
+        $request->setLocale($locale);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
