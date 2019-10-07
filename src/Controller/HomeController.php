a36ff8f4a077aa79a4e5369a4886b47db8994bc7
Carpano Romuald <doc.roms@gmail.com>
2019-10-07 23:46:10 +0200

    feat(controler): add route and templates for differents pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route({
+     *     "fr": "/",
+     *     "en": "/en"
+     * }, name="home")
      */
     public function index()
     {
@@ -19,14 +21,35 @@ class HomeController extends AbstractController
     }
 
     /**
-     * @Route("/{locale}", name="home_international")
+     * @Route({
+     *     "fr": "/a-propos",
+     *     "en": "/en/about"
+     * }, name="about")
      */
-    public function homeInternational(Request $request, $locale)
+    public function about()
     {
-        $request->setLocale($locale);
+        return $this->render('home/about.html.twig');
+    }
 
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+    /**
+     * @Route({
+     *     "fr": "/dernieres-recherches",
+     *     "en": "/en/last-search"
+     * }, name="search")
+     */
+    public function search()
+    {
+        return $this->render('home/search.html.twig');
+    }
+
+    /**
+     * @Route({
+     *     "fr": "/qu-est-ce-que-ecoindex",
+     *     "en": "/en/what-is-ecoindex"
+     * }, name="what_is_it")
+     */
+    public function whatIsIt()
+    {
+        return $this->render('home/what_is_it.html.twig');
     }
 }
