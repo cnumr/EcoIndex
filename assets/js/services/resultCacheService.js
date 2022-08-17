5ed0f74a118bbf135a9361e904c304846f9591e4
Bastien Cornier <bastien@cher-ami.tv>
2022-08-17 12:58:51 +0200

    Add result cache service to store site results

---
+++ assets/js/services/resultCacheService.js
@@ -0,0 +1,55 @@
+export const RESULTS_LOCAL_STORAGE_KEY = "analysisResults";
+
+class ResultCacheService {
+	constructor() {
+		this.results = this.getLocalStorage() || [];
+	}
+
+	get(id) {
+		if (this.results[id]) return this.results[id];
+		// get results list from local storage RESULTS_LOCAL_STORAGE_KEY
+		const results = this.getLocalStorage();
+		if (!results) {
+			console.warn("No results in local storage");
+			return;
+		}
+		// find result from results list with id
+		const result = results.find((result) => result.id === id);
+		if (!result) {
+			console.warn("No result found in local storage from id");
+			return;
+		}
+
+		return result;
+	}
+
+	getLast() {
+		return this.results[this.results.length - 1];
+	}
+
+	add(analysisResultData) {
+		// get item analysisResults from localStorage
+		const analysisResults = JSON.parse(localStorage.getItem(RESULTS_LOCAL_STORAGE_KEY));
+		if (!analysisResults) {
+			// if no item analysisResults in localStorage, create it
+			localStorage.setItem(RESULTS_LOCAL_STORAGE_KEY, JSON.stringify([analysisResultData]));
+		} else {
+			// if item analysisResults in localStorage, add new analysisResultData to it
+			// check if analysisResultData already exist in localStorage with id
+			const analysisResultDataExist = analysisResults.find((analysisResult) => {
+				return analysisResult.id === analysisResultData.id;
+			});
+			if (!analysisResultDataExist) {
+				analysisResults.push(analysisResultData);
+				localStorage.setItem(RESULTS_LOCAL_STORAGE_KEY, JSON.stringify(analysisResults));
+				this.results[analysisResultData.id] = analysisResultData;
+			}
+		}
+	}
+
+	getLocalStorage() {
+		return JSON.parse(localStorage.getItem(RESULTS_LOCAL_STORAGE_KEY));
+	}
+}
+
+export default new ResultCacheService();
